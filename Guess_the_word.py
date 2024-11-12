9962d0b4a28e75281bfe23b41eb12f1930fd48d1
Nathanmwork-sch <nathanmwork3@gmail.com>
2024-11-12 12:17:17 -0700

    Basuc searches for letter for word

--- Guess_the_word.py
+++ Guess_the_word.py
@@ -8,3 +8,19 @@ wordlist = ["apple", "brave", "clamp", "drink", "enter", "flask", "grape", "hast
 
 def randomword():
     return random.choice(wordlist)
+def guesstheword():
+    word = randomword()
+    correctguess = set()
+    icorrect = 0
+    print("guess the word one letter at a time,\nIf the letter is correct i will indicate it\nIf not I will also indicate it\n")
+    while icorrect == 0:
+        guess = input("Guess a single letter: ").lower()
+        #checks for invalid input, If there's a better way to do this please let me know!
+        if len(guess) != 1 or guess =='1' or guess =='2' or guess =='3' or guess =='4' or guess =='5' or guess =='6' or guess =='7' or guess =='8' or guess =='9' or guess =='0':
+            print("Not Ok only one letter")
+            continue
+        if guess in word:
+            correctguess.add(guess)
+            print("Correct guess! ")
+if __name__ == "__main__":
+    guesstheword()
